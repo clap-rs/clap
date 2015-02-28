1cb1dcb73f1cdea39c1ab557e86709606f09927a
Kevin K <kbknapp@gmail.com>
2015-02-28 10:00:24 -0500

    Added documentation

--- src/args/optarg.rs
+++ src/args/optarg.rs
@@ -1,10 +1,32 @@
+/// `OptArg` represents a option argument for command line applications, which is one that
+/// takes an additional value. Option arguments are always preceded by either a `-` 
+/// (single character) or `--` (single word, no spaces) then followed by a space and the
+/// value.
+/// Example:
+/// ```sh
+/// $ myprog -a some --test other --third=file
+/// ```
+/// **NOTE:** The long version may also use the `--argument=value` version too
 pub struct OptArg {
+	/// The unique name of the argument, required
     pub name: &'static str,
+    /// The short version (i.e. single character) of the argument, no preceding `-`
     pub short: Option<char>,
+    /// The long version of the flag (i.e. word) without the preceding `--`
     pub long: Option<&'static str>,
+    /// The string of text that will displayed to the user when the application's
+    /// `help` text is displayed
     pub help: Option<&'static str>,
+    /// If this is a required by default when using the command line program
+    /// i.e. a configuration file that's required for the program to function
+    /// **NOTE:** required by default means, it is required *until* mutually
+    /// exclusive arguments are evaluated.
     pub required: bool,
+    /// A list of names of other arguments that are *required* to be used when 
+    /// this flag is used
     pub requires: Option<Vec<&'static str>>,
+    /// A list of names for other arguments that *may not* be used with this flag
     pub blacklist: Option<Vec<&'static str>>,
+    /// The value provided to the argument by the user
     pub value: Option<String>
 }
--- src/args/posarg.rs
+++ src/args/posarg.rs
@@ -1,4 +1,4 @@
-/// PosArg represents a positional argument, i.e. one that isn't preceded 
+/// `PosArg` represents a positional argument, i.e. one that isn't preceded 
 /// by a `-` or `--`. 
 /// Example: 
 /// ```sh
@@ -7,11 +7,23 @@
 /// where `some_file` is the first positional argument to `myprog`
 /// **NOTE:** The index starts at `1` **NOT** `0`
 pub struct PosArg {
+	/// The unique name of the argument, required
     pub name: &'static str,
+    /// The string of text that will displayed to the user when the application's
+    /// `help` text is displayed
     pub help: Option<&'static str>,
+    /// If this is a required by default when using the command line program
+    /// i.e. a configuration file that's required for the program to function
+    /// **NOTE:** required by default means, it is required *until* mutually
+    /// exclusive arguments are evaluated.
     pub required: bool,
+    /// A list of names of other arguments that are *required* to be used when 
+    /// this flag is used
     pub requires: Option<Vec<&'static str>>,
+    /// A list of names for other arguments that *may not* be used with this flag
     pub blacklist: Option<Vec<&'static str>>,
+    /// The value provided to the argument by the user
     pub value: Option<String>,
+    /// The index of the argument
     pub index: u8 
 }
