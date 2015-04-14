8752700fbb30e89ee68adbce24489ae9a24d33a9
Kevin K <kbknapp@gmail.com>
2015-04-14 12:06:15 -0400

    feat(macros): add convenience macro to get a typed value

--- src/macros.rs
+++ src/macros.rs
@@ -28,3 +28,19 @@ macro_rules! for_match {
 		}
 	};
 }
+
+/// Convenience macro getting a typed value
+#[macro_export]
+macro_rules! value_t {
+	($m:ident.value_of($v:expr), $t:ty) => {
+		match $m.value_of($v) {
+			Some(v) => {
+				match v.parse::<$t>() {
+					Ok(val) => Ok(val),
+					Err(_)  => Err(format!("{} isn't a valid {}",v,stringify!($t))),
+				}
+			},
+			None => Err(format!("Argument not found"))
+		}
+	};
+}
