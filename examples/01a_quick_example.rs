ddb0987d0ce95bff9f9e1893044c7c5532378a76
Alexander Kuvaev <alexander@kuvaev.me>
2015-09-09 13:06:54 +0300

    wip: minor fixes

--- examples/01a_quick_example.rs
+++ examples/01a_quick_example.rs
@@ -8,6 +8,8 @@ fn main() {
     // far less verbose that shown in 01b_QuickExample.rs, but is more readable. The downside is you cannot set
     // the more advanced configuration options using this method (well...actually you can, you'll see ;) )
     // 
+    // The example below is functionally identical to the 01b_quick_example.rs and 01c_quick_example.rs
+    //
     // Create an application with 5 possible arguments (2 auto generated) and 2 subcommands (1 auto generated)
     //    - A config file
     //        + Uses "-c filename" or "--config filename"
