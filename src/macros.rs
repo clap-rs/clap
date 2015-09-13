ffe14588826db70a494378d51c2a33e1f9962be0
Dabo Ross <daboross@daboross.net>
2015-09-12 20:22:01 -0700

    Replace crate_version!() macro with simpler call
    As of https://github.com/rust-lang/cargo/pull/1094, cargo publishes the full crate version as `CARGO_PKG_VERSION`, rather than *just* the parts of it.
    
    This replaces the more complicated call with simply `env!("CARGO_PKG_VERSION").to_owned()`.

--- src/macros.rs
+++ src/macros.rs
@@ -732,11 +732,7 @@ macro_rules! arg_enum {
 #[macro_export]
 macro_rules! crate_version {
     () => {
-        format!("{}.{}.{}{}",
-            env!("CARGO_PKG_VERSION_MAJOR"),
-            env!("CARGO_PKG_VERSION_MINOR"),
-            env!("CARGO_PKG_VERSION_PATCH"),
-            option_env!("CARGO_PKG_VERSION_PRE").unwrap_or(""))
+        env!("CARGO_PKG_VERSION").to_owned()
     }
 }
 
