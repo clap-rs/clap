19538b9c22fed1acb6bc6018ad98e98fbc23366f
Kevin K <kbknapp@gmail.com>
2015-10-29 07:16:03 -0400

    chore: fixes features declarations

--- src/lib.rs
+++ src/lib.rs
@@ -624,12 +624,12 @@
 #![cfg_attr(feature = "lints", allow(explicit_iter_loop))]
 #![cfg_attr(feature = "lints", allow(should_implement_trait))]
 #![cfg_attr(feature = "lints", deny(warnings))]
+#![cfg_attr(not(any(feature = "lints", feature = "nightly")), deny(unstable_features))]
 #![deny(missing_docs,
         missing_debug_implementations,
         missing_copy_implementations,
         trivial_casts, trivial_numeric_casts,
         unsafe_code,
-        unstable_features,
         unused_import_braces,
         unused_qualifications)]
 
