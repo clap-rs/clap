b261783c718060ae0ee737de9b06b95bc851ea54
Kevin K <kbknapp@gmail.com>
2015-10-29 06:44:52 -0400

    refactor(UsageParser): moves code for readability

--- src/usageparser.rs
+++ src/usageparser.rs
@@ -25,7 +25,104 @@ impl<'u> UsageParser<'u> {
         }
     }
 
+    fn name(&mut self, c: char) -> Option<UsageToken<'u>> {
+        if self.e != 0 {
+            self.e += 1;
+        }
+        self.s = self.e + 1;
+        let closing = match c {
+            '[' => ']',
+            '<' => '>',
+            _ => unreachable!(),
+        };
+        while let Some(c) = self.chars.next() {
+            self.e += 1;
+            if c == closing {
+                break;
+            }
+        }
+        if self.e > self.usage.len() {
+            return None;
+        }
 
+        let name = &self.usage[self.s..self.e];
+
+        Some(UsageToken::Name(name,
+                              if c == '<' {
+                                  Some(true)
+                              } else {
+                                  None
+                              }))
+    }
+
+    fn help(&mut self) -> Option<UsageToken<'u>> {
+        self.s = self.e + 2;
+        self.e = self.usage.len() - 1;
+
+        while let Some(_) = self.chars.next() {
+            continue;
+        }
+
+        Some(UsageToken::Help(&self.usage[self.s..self.e]))
+    }
+
+    fn long_arg(&mut self) -> Option<UsageToken<'u>> {
+        if self.e != 1 {
+            self.e += 1;
+        }
+
+        self.s = self.e + 1;
+
+        while let Some(c) = self.chars.next() {
+            self.e += 1;
+            if c == ' ' || c == '=' || c == '.' {
+                break;
+            }
+        }
+
+        if self.e > self.usage.len() {
+            return None;
+        } else if self.e == self.usage.len() - 1 {
+            return Some(UsageToken::Long(&self.usage[self.s..]));
+        }
+
+        Some(UsageToken::Long(&self.usage[self.s..self.e]))
+    }
+
+    fn short_arg(&mut self, c: char) -> Option<UsageToken<'u>> {
+        // When short is first don't increment e
+        if self.e != 1 {
+            self.e += 1;
+        }
+        if !c.is_alphanumeric() {
+            return None;
+        }
+        Some(UsageToken::Short(c))
+    }
+
+    fn multiple(&mut self) -> bool {
+        self.e += 1;
+        let mut mult = false;
+        for _ in 0..2 {
+            self.e += 1;
+            match self.chars.next() {
+                // longs consume one '.' so they match '.. ' whereas shorts can
+                // match '...'
+                Some('.') | Some(' ') => {
+                    mult = true;
+                }
+                _ => {
+                    // if there is no help or following space all we can match is '..'
+                    if self.e == self.usage.len() - 1 {
+                        mult = true;
+                    }
+                    break;
+                }
+            }
+        }
+
+        mult
+    }
 }
 
 impl<'u> Iterator for UsageParser<'u> {
@@ -35,80 +132,19 @@ impl<'u> Iterator for UsageParser<'u> {
         loop {
             match self.chars.next() {
                 Some(c) if c == '[' || c == '<' => {
-                    // self.s = self.e + 1;
-                    if self.e != 0 {
-                        self.e += 1;
-                    }
-                    self.s = self.e + 1;
-                    let closing = match c {
-                        '[' => ']',
-                        '<' => '>',
-                        _ => unreachable!(),
-                    };
-                    while let Some(c) = self.chars.next() {
-                        self.e += 1;
-                        if c == closing {
-                            break;
-                        }
-                    }
-                    if self.e > self.usage.len() {
-                        return None;
-                    }
-
-                    let name = &self.usage[self.s..self.e];
-
-                    return Some(UsageToken::Name(name,
-                                                 if c == '<' {
-                                                     Some(true)
-                                                 } else {
-                                                     None
-                                                 }));
+                    return self.name(c);
                 }
                 Some('\'') => {
-                    self.s = self.e + 2;
-                    self.e = self.usage.len() - 1;
-
-                    while let Some(_) = self.chars.next() {
-                        continue;
-                    }
-
-                    return Some(UsageToken::Help(&self.usage[self.s..self.e]));
+                    return self.help();
                 }
                 Some('-') => {
                     self.e += 1;
                     match self.chars.next() {
                         Some('-') => {
-                            if self.e != 1 {
-                                self.e += 1;
-                            }
-
-                            self.s = self.e + 1;
-
-                            while let Some(c) = self.chars.next() {
-                                self.e += 1;
-                                if c == ' ' || c == '=' || c == '.' {
-                                    break;
-                                }
-                            }
-                            if self.e > self.usage.len() {
-                                return None;
-                            }
-
-                            if self.e == self.usage.len() - 1 {
-                                return Some(UsageToken::Long(&self.usage[self.s..]));
-                            }
-                            return Some(UsageToken::Long(&self.usage[self.s..self.e]));
+                            return self.long_arg();
                         }
                         Some(c) => {
-                            // When short is first don't increment e
-                            if self.e != 1 {
-                                self.e += 1;
-                            }
-                            // Short
-                            if !c.is_alphanumeric() {
-                                return None;
-                            }
-                            return Some(UsageToken::Short(c));
+                            return self.short_arg(c);
                         }
                         _ => {
                             return None;
@@ -116,26 +152,7 @@ impl<'u> Iterator for UsageParser<'u> {
                     }
                 }
                 Some('.') => {
-                    self.e += 1;
-                    let mut mult = false;
-                    for _ in 0..2 {
-                        self.e += 1;
-                        match self.chars.next() {
-                            // longs consume one '.' so they match '.. ' whereas shorts can
-                            // match '...'
-                            Some('.') | Some(' ') => {
-                                mult = true;
-                            }
-                            _ => {
-                                // if there is no help or following space all we can match is '..'
-                                if self.e == self.usage.len() - 1 {
-                                    mult = true;
-                                }
-                                break;
-                            }
-                        }
-                    }
-                    if mult {
+                    if self.multiple() {
                         return Some(UsageToken::Multiple);
                     }
                 }
