52ca6505b4fec7b5c2d53d160c072d395eb21da6
Kevin K <kbknapp@gmail.com>
2016-05-13 18:52:29 -0400

    docs: makes all publicly available types viewable in docs
    Some types weren't viewable in the docs, such as `Values`, `OsValues`,
    and `ArgSettings`. All these types should now be browsable in the
    docs page.
    
    Relates to #505

--- src/lib.rs
+++ src/lib.rs
@@ -417,7 +417,7 @@ extern crate vec_map;
 
 #[cfg(feature = "yaml")]
 pub use yaml_rust::YamlLoader;
-pub use args::{Arg, ArgGroup, ArgMatches, ArgSettings, SubCommand};
+pub use args::{Arg, ArgGroup, ArgMatches, ArgSettings, SubCommand, Values, OsValues};
 pub use app::{App, AppSettings};
 pub use fmt::Format;
 pub use errors::{Error, ErrorKind, Result};
