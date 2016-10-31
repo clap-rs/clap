44f6b1edbfef70ee94a641b3aa0c334baf0a1c2b
Kevin K <kbknapp@gmail.com>
2016-10-31 00:35:13 -0400

    tests: updates conflict tests to new more precise output

--- tests/conflicts.rs
+++ tests/conflicts.rs
@@ -5,10 +5,10 @@ include!("../clap-test.rs");
 
 use clap::{App, Arg, ErrorKind, ArgGroup};
 
-static CONFLICT_ERR: &'static str = "error: The argument '--flag' cannot be used with '-F'
+static CONFLICT_ERR: &'static str = "error: The argument '-F' cannot be used with '--flag'
 
 USAGE:
-    clap-test <positional> <positional2> -F --long-option-2 <option2>
+    clap-test <positional> <positional2> --flag --long-option-2 <option2>
 
 For more information try --help";
 
