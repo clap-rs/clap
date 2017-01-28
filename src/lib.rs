12282e10510754ff5f455e65d638648df3643ee9
Martin Geisler <martin@geisler.net>
2017-01-28 12:42:09 +0100

    feat: use textwrap crate for wrapping help texts
    The textwrap crate uses a simpler linear-time algorithm for wrapping
    the text. The current algorithm in wrap_help uses several O(n) calls
    to String::insert and String::remove, which makes it potentially
    quadratic in complexity.
    
    Comparing the 05_ripgrep benchmark at commits textwrap~2 and textwrap
    gives this result on my machine:
    
     name              before ns/iter  after ns/iter  diff ns/iter   diff %
     build_app_long    22,101          21,099               -1,002   -4.53%
     build_app_short   22,138          21,205                 -933   -4.21%
     build_help_long   514,265         284,467            -229,798  -44.68%
     build_help_short  85,720          85,693                  -27   -0.03%
     parse_clean       23,471          22,859                 -612   -2.61%
     parse_complex     29,535          28,919                 -616   -2.09%
     parse_lots        422,815         414,577              -8,238   -1.95%
    
    As part of this commit, the wrapping_newline_chars test was updated.
    The old algorithm had a subtle bug where it would break lines too
    early. That is, it wrapped the text like
    
        ARGS:
            <mode>    x, max, maximum   20 characters, contains
                      symbols.
                      l, long           Copy-friendly,
                      14 characters, contains symbols.
                      m, med, medium    Copy-friendly, 8
                      characters, contains symbols.";
    
    when it should really have wrapped it like
    
        ARGS:
            <mode>    x, max, maximum   20 characters, contains
                      symbols.
                      l, long           Copy-friendly, 14
                      characters, contains symbols.
                      m, med, medium    Copy-friendly, 8
                      characters, contains symbols.";
    
    Notice how the word "14" was incorrectly moved to the next line. There
    is clearly room for the word on the line with the "l, long" option
    since there is room for "contains" just above it.
    
    I'm not sure why this is, but the algorithm in textwrap handles this
    case correctly.

--- src/lib.rs
+++ src/lib.rs
@@ -541,6 +541,7 @@ extern crate bitflags;
 extern crate vec_map;
 #[cfg(feature = "wrap_help")]
 extern crate term_size;
+extern crate textwrap;
 extern crate unicode_segmentation;
 #[cfg(feature = "color")]
 extern crate atty;
