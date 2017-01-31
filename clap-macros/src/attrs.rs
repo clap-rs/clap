a6587a9a632e659c81ee524eb498e9c362da7ee4
Kevin K <kbknapp@gmail.com>
2017-01-30 19:56:58 -0500

    style(clap_macros): simplifies and removes some rightward drift

--- clap-macros/src/attrs.rs
+++ clap-macros/src/attrs.rs
@@ -1,5 +1,5 @@
 use std::cell::RefCell;
-use std::collections::{ BTreeMap, HashMap };
+use std::collections::{BTreeMap, HashMap};
 
 use syn;
 
@@ -18,56 +18,58 @@ pub struct FieldAttributes {
 
 impl Default for Attributes {
     fn default() -> Self {
-        Attributes { summary: "".into(), docs: "".into(), map: BTreeMap::new() }
+        Attributes {
+            summary: "".into(),
+            docs: "".into(),
+            map: BTreeMap::new(),
+        }
     }
 }
 
 impl Attributes {
-    pub fn new() -> Self {
-        Default::default()
-    }
+    pub fn new() -> Self { Default::default() }
 
     pub fn from_attrs(attrs: &[syn::Attribute]) -> Self {
+        use syn::NestedMetaItem::*;
+        use syn::MetaItem::*;
         let mut claps = BTreeMap::new();
         for attr in attrs {
             if let syn::MetaItem::List(ref ident, ref values) = attr.value {
-                if ident == "clap" {
-                    for value in values {
-                        match *value {
-                            syn::NestedMetaItem::MetaItem(ref item) => match *item {
-                                syn::MetaItem::NameValue(ref name, ref value) => {
-                                    let &mut (_, ref mut attr) = claps.entry(name.to_string()).or_insert((RefCell::new(0), Attribute::new(name.to_string())));
-                                    attr.push(value.clone());
-                                }
-                                syn::MetaItem::Word(ref name) => {
-                                    let &mut (_, ref mut attr) = claps.entry(name.to_string()).or_insert((RefCell::new(0), Attribute::new(name.to_string())));
-                                    attr.push(syn::Lit::Bool(true));
-                                }
-                                syn::MetaItem::List(ref ident, ref values) => {
-                                    let &mut (_, ref mut attr) = claps.entry(ident.as_ref().to_string()).or_insert((RefCell::new(0), Attribute::new(ident.as_ref().to_string())));
-                                    for value in values {
-                                        match *value {
-                                            syn::NestedMetaItem::MetaItem(ref item) => match *item {
-                                                syn::MetaItem::Word(ref name) => {
-                                                    attr.push(name.as_ref().into());
-                                                }
-                                                syn::MetaItem::NameValue(..) => {
-                                                    panic!("Invalid clap attribute {} named value in sublist not supported", quote!(#attr).to_string().replace(" ", ""));
-                                                }
-                                                syn::MetaItem::List(..) => {
-                                                    panic!("Invalid clap attribute {} sublist in sublist not supported", quote!(#attr).to_string().replace(" ", ""));
-                                                }
-                                            },
-                                            syn::NestedMetaItem::Literal(_) => {
-                                                panic!("Invalid clap attribute {} literal value not supported", quote!(#attr).to_string().replace(" ", ""));
-                                            },
-                                        }
+                if ident != "clap" {
+                    panic!("Attribute other than #[clap(..)] found");
+                }
+                for value in values {
+                    match *value {
+                        // #[foo = "bar"]
+                        MetaItem(NameValue(ref name, ref value)) => {
+                            let &mut (_, ref mut attr) = claps.entry(name.to_string())
+                                .or_insert((RefCell::new(0), Attribute::new(name.to_string())));
+                            attr.push(value.clone());
+                        }
+                        // #[foo]
+                        MetaItem(Word(ref name)) => {
+                            let &mut (_, ref mut attr) = claps.entry(name.to_string())
+                                .or_insert((RefCell::new(0), Attribute::new(name.to_string())));
+                            attr.push(syn::Lit::Bool(true));
+                        }
+                        // #[derive(..)]
+                        MetaItem(List(ref ident, ref values)) => {
+                            let &mut (_, ref mut attr) = claps.entry(ident.as_ref().to_string())
+                                .or_insert((RefCell::new(0),
+                                            Attribute::new(ident.as_ref().to_string())));
+                            for value in values {
+                                match *value {
+                                    MetaItem(Word(ref name)) => attr.push(name.as_ref().into()),
+                                    _ => {
+                                        panic!("Invalid clap attribute {} literal value not supported",
+                                               quote!(#attr).to_string().replace(" ", ""))
                                     }
                                 }
-                            },
-                            syn::NestedMetaItem::Literal(_) => {
-                                panic!("Invalid clap attribute {} literal value not supported", quote!(#attr).to_string().replace(" ", ""));
-                            },
+                            }
+                        }
+                        _ => {
+                            panic!("Invalid clap attribute {} literal value not supported",
+                                   quote!(#attr).to_string().replace(" ", ""))
                         }
                     }
                 }
@@ -80,7 +82,8 @@ impl Attributes {
                 syn::MetaItem::NameValue(_, syn::Lit::Str(ref doc, _)) => doc,
                 _ => unreachable!(),
             })
-            .fold(String::new(), |docs, line| docs + line.trim_left_matches('/').trim() + "\n");
+            .fold(String::new(),
+                  |docs, line| docs + line.trim_left_matches('/').trim() + "\n");
 
         let index = docs.find("\n\n");
         let (summary, docs) = if let Some(index) = index {
@@ -91,17 +94,28 @@ impl Attributes {
             (docs, "".into())
         };
 
-        Attributes { summary: summary, docs: docs, map: claps }
+        Attributes {
+            summary: summary,
+            docs: docs,
+            map: claps,
+        }
     }
 
     pub fn check_used(&self, name: &str, field: Option<&str>) {
         for (ref attr, &(ref counter, _)) in &self.map {
             if *counter.borrow() == 0 {
                 match field {
-                    Some(field) =>
-                        println!("clap-macros: unexpected attribute '{}' on field '{}' of struct '{}'", attr, field, name),
-                    None =>
-                        println!("clap-macros: unexpected attribute '{}' on struct '{}'", attr, name),
+                    Some(field) => {
+                        println!("clap-macros: unexpected attribute '{}' on field '{}' of struct '{}'",
+                                 attr,
+                                 field,
+                                 name)
+                    }
+                    None => {
+                        println!("clap-macros: unexpected attribute '{}' on struct '{}'",
+                                 attr,
+                                 name)
+                    }
                 }
             }
         }
@@ -116,16 +130,16 @@ impl Attributes {
         }
     }
 
-    pub fn get_bool(&self, key: &str) -> bool {
-        self.get(key).map(|a| a.into()).unwrap_or(false)
-    }
+    pub fn get_bool(&self, key: &str) -> bool { self.get(key).map(|a| a.into()).unwrap_or(false) }
 }
 
 impl FieldAttributes {
     pub fn check_used(&self, name: &str) {
         for (ref field, &(ref counter, ref attrs)) in &self.map {
             if *counter.borrow() == 0 {
-                panic!("clap-macros: didn't access attributes for field '{}' on struct '{}' for some reason", field, name);
+                panic!("clap-macros: didn't access attributes for field '{}' on struct '{}' for some reason",
+                       field,
+                       name);
             }
             attrs.check_used(name, Some(field.as_ref()));
         }
@@ -143,21 +157,25 @@ impl FieldAttributes {
 
 /// Extracts all clap attributes of the form #[clap(i = V)]
 pub fn extract_attrs(ast: &syn::MacroInput) -> (Attributes, FieldAttributes) {
+    use syn::Body::*;
+    use syn::VariantData::*;
     let empty = Attributes::new();
     let root_attrs = Attributes::from_attrs(&ast.attrs);
     let field_attrs = match ast.body {
-        syn::Body::Struct(syn::VariantData::Struct(ref fields)) => {
-            fields
-                .iter()
-                .map(|field| (field.ident.clone().unwrap(), (RefCell::new(0), extract_attrs_inner(&field.attrs))))
+        Struct(Struct(ref fields)) => {
+            fields.iter()
+                .map(|field| {
+                    (field.ident.clone().unwrap(),
+                     (RefCell::new(0), Attributes::from_attrs(&field.attrs)))
+                })
                 .collect()
         }
-        syn::Body::Struct(syn::VariantData::Tuple(_)) => {
-            panic!("TODO: tuple struct unsupported msg")
-        }
-        syn::Body::Struct(syn::VariantData::Unit) | syn::Body::Enum(_) => {
-            HashMap::new()
-        }
+        Struct(Tuple(_)) => panic!("TODO: tuple struct unsupported msg"),
+        Struct(Unit) | Enum(_) => HashMap::new(),
     };
-    (root_attrs, FieldAttributes { empty: empty, map: field_attrs })
+    (root_attrs,
+     FieldAttributes {
+         empty: empty,
+         map: field_attrs,
+     })
 }
