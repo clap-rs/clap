6bf5bf5bee0b290a2ad67d51c2cf3551ea82993e
CrazyMerlyn <crazy.d.merlyn@gmail.com>
2017-03-22 13:19:23 +0530

    fix(yaml): adds support for loading author info from yaml
    fix(yaml): adds support for loading author info from yaml

--- tests/yaml.rs
+++ tests/yaml.rs
@@ -24,3 +24,17 @@ fn help_message() {
     assert!(help_string.contains(
         "-h, --help             prints help with a nonstandard description\n"));
 }
+
+#[test]
+fn author() {
+    let yml = load_yaml!("app.yml");
+    let mut app = App::from_yaml(yml);
+    // Generate the full help message!
+    let _ = app.get_matches_from_safe_borrow(Vec::<String>::new());
+
+    let mut help_buffer = Vec::new();
+    app.write_help(&mut help_buffer).unwrap();
+    let help_string = String::from_utf8(help_buffer).unwrap();
+    assert!(help_string.contains(
+        "Kevin K. <kbknapp@gmail.com>"));
+}
