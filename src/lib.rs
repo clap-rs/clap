71dabba3ea0a17c88b0e2199c9d99f0acbf3bc17
Michael Daffin <michael@daffin.io>
2017-04-07 09:58:30 +0100

    docs: Fix a typo the minimum rust version required

--- src/lib.rs
+++ src/lib.rs
@@ -500,7 +500,7 @@
 //! `clap` will officially support current stable Rust, minus two releases, but may work with prior
 //! releases as well. For example, current stable Rust at the time of this writing is 1.13.0,
 //! meaning `clap` is guaranteed to compile with 1.11.0 and beyond. At the 1.14.0 release, `clap`
-//! will be guaranteed to compile with 2.12.0 and beyond, etc.
+//! will be guaranteed to compile with 1.12.0 and beyond, etc.
 //! 
 //! Upon bumping the minimum version of Rust (assuming it's within the stable-2 range), it *must* be
 //! clearly annotated in the `CHANGELOG.md`
