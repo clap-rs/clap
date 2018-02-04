3e7d843d8268f627776fb15a9f356663af35be7e
Guillaume Pinot <texitoi@texitoi.eu>
2018-02-04 19:27:45 +0100

    Add test for the options

---
+++ tests/options.rs
@@ -0,0 +1,85 @@
+// Copyright (c) 2018 Guillaume Pinot <texitoi(a)texitoi.eu>
+//
+// This work is free. You can redistribute it and/or modify it under
+// the terms of the Do What The Fuck You Want To Public License,
+// Version 2, as published by Sam Hocevar. See the COPYING file for
+// more details.
+
+#[macro_use]
+extern crate structopt;
+
+use structopt::StructOpt;
+
+#[test]
+fn required_option() {
+    #[derive(StructOpt, PartialEq, Debug)]
+    struct Opt {
+        #[structopt(short = "a", long = "arg")]
+        arg: i32,
+    }
+    assert_eq!(Opt { arg: 42 },
+               Opt::from_clap(&Opt::clap().get_matches_from(&["test", "-a42"])));
+    assert_eq!(Opt { arg: 42 },
+               Opt::from_clap(&Opt::clap().get_matches_from(&["test", "-a", "42"])));
+    assert_eq!(Opt { arg: 42 },
+               Opt::from_clap(&Opt::clap().get_matches_from(&["test", "--arg", "42"])));
+    assert!(Opt::clap().get_matches_from_safe(&["test"]).is_err());
+    assert!(Opt::clap().get_matches_from_safe(&["test", "-a42", "-a24"]).is_err());
+}
+
+#[test]
+fn optional_option() {
+    #[derive(StructOpt, PartialEq, Debug)]
+    struct Opt {
+        #[structopt(short = "a")]
+        arg: Option<i32>,
+    }
+    assert_eq!(Opt { arg: Some(42) },
+               Opt::from_clap(&Opt::clap().get_matches_from(&["test", "-a42"])));
+    assert_eq!(Opt { arg: None },
+               Opt::from_clap(&Opt::clap().get_matches_from(&["test"])));
+    assert!(Opt::clap().get_matches_from_safe(&["test", "-a42", "-a24"]).is_err());
+}
+
+#[test]
+fn option_with_default() {
+    #[derive(StructOpt, PartialEq, Debug)]
+    struct Opt {
+        #[structopt(short = "a", default_value = "42")]
+        arg: i32,
+    }
+    assert_eq!(Opt { arg: 24 },
+               Opt::from_clap(&Opt::clap().get_matches_from(&["test", "-a24"])));
+    assert_eq!(Opt { arg: 42 },
+               Opt::from_clap(&Opt::clap().get_matches_from(&["test"])));
+    assert!(Opt::clap().get_matches_from_safe(&["test", "-a42", "-a24"]).is_err());
+}
+
+#[test]
+fn option_with_raw_default() {
+    #[derive(StructOpt, PartialEq, Debug)]
+    struct Opt {
+        #[structopt(short = "a", raw(default_value = "\"42\""))]
+        arg: i32,
+    }
+    assert_eq!(Opt { arg: 24 },
+               Opt::from_clap(&Opt::clap().get_matches_from(&["test", "-a24"])));
+    assert_eq!(Opt { arg: 42 },
+               Opt::from_clap(&Opt::clap().get_matches_from(&["test"])));
+    assert!(Opt::clap().get_matches_from_safe(&["test", "-a42", "-a24"]).is_err());
+}
+
+#[test]
+fn options() {
+    #[derive(StructOpt, PartialEq, Debug)]
+    struct Opt {
+        #[structopt(short = "a", long = "arg")]
+        arg: Vec<i32>,
+    }
+    assert_eq!(Opt { arg: vec![24] },
+               Opt::from_clap(&Opt::clap().get_matches_from(&["test", "-a24"])));
+    assert_eq!(Opt { arg: vec![] },
+               Opt::from_clap(&Opt::clap().get_matches_from(&["test"])));
+    assert_eq!(Opt { arg: vec![24, 42] },
+               Opt::from_clap(&Opt::clap().get_matches_from(&["test", "-a24", "--arg", "42"])));
+}
