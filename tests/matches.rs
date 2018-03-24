a2a58c377c585ddd01db98545af9b428d99bf469
Filippo Costa <filippocosta.italy@gmail.com>
2018-03-25 01:12:01 +0200

    Add an ArgMatches iterating test

---
+++ tests/matches.rs
@@ -0,0 +1,16 @@
+#![cfg(feature="iter")]
+
+extern crate clap;
+
+use clap::{App, Arg};
+
+#[test]
+fn iter_matches_empty() {
+    let res = App::new("prog")
+        .arg(Arg::with_name("cfg")
+            .takes_value(true)
+            .long("config"))
+        .get_matches_from_safe(vec!["prog"]);
+    assert!(res.is_ok());
+    assert_eq!(res.unwrap().args.iter().count(), 0);
+}
