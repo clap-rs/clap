1b257f54b88b670dfe51ee4530207480fbd69ec0
Kevin K <kbknapp@gmail.com>
2018-07-08 22:08:20 -0400

    tests: adds a basic test to use while developing

---
+++ tests/basic.rs
@@ -0,0 +1,33 @@
+// Copyright 2018 Guillaume Pinot (@TeXitoi) <texitoi@texitoi.eu>,
+// Kevin Knapp (@kbknapp) <kbknapp@gmail.com>, and
+// Andrew Hobden (@hoverbear) <andrew@hoverbear.org>
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+//
+// This work was derived from Structopt (https://github.com/TeXitoi/structopt)
+// commit#ea76fa1b1b273e65e3b0b1046643715b49bec51f which is licensed under the
+// MIT/Apache 2.0 license.
+
+#[macro_use]
+extern crate clap;
+
+use clap::Clap;
+
+#[test]
+fn basic() {
+    #[derive(Clap, PartialEq, Debug)]
+    struct Opt {
+        #[clap(short = "a", long = "arg")]
+        arg: Vec<i32>,
+    }
+    assert_eq!(Opt { arg: vec![24] }, Opt::parse_from(&["test", "-a24"]));
+    assert_eq!(Opt { arg: vec![] }, Opt::parse_from(&["test"]));
+    assert_eq!(
+        Opt { arg: vec![24, 42] },
+        Opt::parse_from(&["test", "-a24", "--arg", "42"])
+    );
+}
