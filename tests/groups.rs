2699d9e51e7eadc258ba64c4e347c5d1fef61343
Janik Rabe <info@janikrabe.com>
2019-02-04 09:38:12 +0200

    fix: add debug assertion for missing args in subcommand ArgGroup
    In debug mode, assert that all arguments in a subcommand's required
    ArgGroup exist when printing the subcommand's help message. The previous
    behavior was an expectation failure with the generic INTERNAL_ERROR_MSG.
    
    See-also: 7ad123e2c02577 (introduced relevant assertion)

--- tests/groups.rs
+++ tests/groups.rs
@@ -3,7 +3,7 @@ extern crate regex;
 
 include!("../clap-test.rs");
 
-use clap::{App, Arg, ArgGroup, ErrorKind};
+use clap::{App, Arg, ArgGroup, ErrorKind, SubCommand};
 
 static REQ_GROUP_USAGE: &'static str = "error: The following required arguments were not provided:
     <base|--delete>
@@ -51,6 +51,20 @@ fn non_existing_arg() {
             .args(&["flg", "color"])
             .required(true))
         .get_matches_from_safe(vec![""]);
+}
+
+#[test]
+#[should_panic(expected = "The group 'c' contains the arg 'd' that doesn't actually exist.")]
+fn non_existing_arg_in_subcommand_help() {
+    let _ = App::new("a")
+        .subcommand(
+            SubCommand::with_name("b")
+                .group(
+                    ArgGroup::with_name("c")
+                        .args(&["d"])
+                        .required(true),
+                )
+        ).get_matches_from_safe(vec!["a", "help", "b"]);
 }
 
 #[test]
