d4296bb4289366bdd7ba0ace9729f7263e0802f9
Katharina <kookie@spacekookie.de>
2019-03-27 15:06:42 +0100

    Revert "Upgrading yaml-rust to 0.4.2. Solving issue #1387"
    This reverts commit 6508fb123c83e7c320fa02f761dc3ae2a108312d.

--- src/lib.rs
+++ src/lib.rs
@@ -548,8 +548,6 @@ extern crate unicode_width;
 extern crate vec_map;
 #[cfg(feature = "yaml")]
 extern crate yaml_rust;
-#[cfg(feature = "yaml")]
-extern crate linked_hash_map;
 
 #[cfg(feature = "yaml")]
 pub use yaml_rust::YamlLoader;
