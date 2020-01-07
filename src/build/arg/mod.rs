7781fd881307b9f8447fccbaadc31d941d215761
Gregor Pfeifer <gpfeifer@dxc.com>
2020-01-07 20:16:59 +0100

    refactor: Rename Arg::conflicts_with_everything to Arg::exclusive (#1583) (#1624)
    https://github.com/clap-rs/clap/pull/1624#issuecomment-571372359

--- src/build/arg/mod.rs
+++ src/build/arg/mod.rs
@@ -785,8 +785,12 @@ impl<'help> Arg<'help> {
     /// only need to be set for one of the two arguments, they do not need to be set for each.
     ///
     /// **NOTE:** Defining a conflict is two-way, but does *not* need to defined for both arguments
-    /// (i.e. if A conflicts with B, defining A.conflicts_with(B) is sufficient. You do not need
+    /// (i.e. if A conflicts with B, defining A.conflicts_with(B) is sufficient. You do not
     /// need to also do B.conflicts_with(A))
+    ///
+    /// **NOTE:** [`Arg::conflicts_with_all(names)`] allows specifying an argument which conflicts with more than one argument.
+    ///
+    /// **NOTE** [`Arg::exclusive(true)`] allows specifying an argument which conflicts with every other argument.
     ///
     /// # Examples
     ///
@@ -815,6 +819,10 @@ impl<'help> Arg<'help> {
     /// assert!(res.is_err());
     /// assert_eq!(res.unwrap_err().kind, ErrorKind::ArgumentConflict);
     /// ```
+    ///     
+    /// [`Arg::conflicts_with_all(names)`]: ./struct.Arg.html#method.conflicts_with_all
+    /// [`Arg::exclusive(true)`]: ./struct.Arg.html#method.exclusive
+
     pub fn conflicts_with<T: Key>(mut self, arg_id: T) -> Self {
         let name = arg_id.key();
         if let Some(ref mut vec) = self.blacklist {
@@ -834,6 +842,8 @@ impl<'help> Arg<'help> {
     /// **NOTE:** Defining a conflict is two-way, but does *not* need to defined for both arguments
     /// (i.e. if A conflicts with B, defining A.conflicts_with(B) is sufficient. You do not need
     /// need to also do B.conflicts_with(A))
+    ///
+    /// **NOTE:** [`Arg::exclusive(true)`] allows specifying an argument which conflicts with every other argument.
     ///
     /// # Examples
     ///
@@ -866,6 +876,8 @@ impl<'help> Arg<'help> {
     /// assert_eq!(res.unwrap_err().kind, ErrorKind::ArgumentConflict);
     /// ```
     /// [`Arg::conflicts_with`]: ./struct.Arg.html#method.conflicts_with
+    /// [`Arg::exclusive(true)`]: ./struct.Arg.html#method.exclusive
+
     pub fn conflicts_with_all(mut self, names: &[&str]) -> Self {
         if let Some(ref mut vec) = self.blacklist {
             for s in names {
@@ -885,7 +897,7 @@ impl<'help> Arg<'help> {
     /// ```rust
     /// # use clap::Arg;
     /// Arg::with_name("config")
-    ///     .conflicts_with_everything()
+    ///     .exclusive(true)
     /// # ;
     /// ```
     /// **NOTE:** If using YAML the above example should be laid out as follows
@@ -903,7 +915,7 @@ impl<'help> Arg<'help> {
     /// let res = App::new("prog")
     ///     .arg(Arg::with_name("exclusive")
     ///         .takes_value(true)
-    ///         .conflicts_with_everything()
+    ///         .exclusive(true)
     ///         .long("exclusive"))
     ///     .arg(Arg::with_name("debug")
     ///         .long("debug"))
@@ -916,8 +928,8 @@ impl<'help> Arg<'help> {
     /// assert!(res.is_err());
     /// assert_eq!(res.unwrap_err().kind, ErrorKind::ArgumentConflict);
     /// ```
-    pub fn conflicts_with_everything(mut self) -> Self {
-        self.exclusive = true;
+    pub fn exclusive(mut self, exclusive: bool) -> Self {
+        self.exclusive = exclusive;
         self
     }
 
@@ -4147,13 +4159,6 @@ impl<'help> Arg<'help> {
             debugln!("PosBuilder:name_no_brackets: just name");
             Cow::Borrowed(self.name)
         }
-    }
-
-    // Used by yaml
-    #[allow(dead_code)]
-    fn exclusive(mut self, exclusive: bool) -> Self {
-        self.exclusive = exclusive;
-        self
     }
 }
 
