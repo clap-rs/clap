506f5e49658c5cf82227dd6d6e58ac333be5fef1
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2020-02-02 08:50:40 +0100

    Added tests for cargo macros, fixes #1478

---
+++ tests/app_from_crate.rs
@@ -0,0 +1,26 @@
+use clap::{app_from_crate, ErrorKind};
+
+static EVERYTHING: &str = "clap {{version}}
+Kevin K. <kbknapp@gmail.com>
+A simple to use, efficient, and full-featured Command Line Argument Parser
+
+USAGE:
+    clap
+
+FLAGS:
+    -h, --help       Prints help information
+    -V, --version    Prints version information
+";
+
+#[test]
+fn app_from_crate() {
+    let res = app_from_crate!().try_get_matches_from(vec!["clap", "--help"]);
+
+    assert!(res.is_err());
+    let err = res.unwrap_err();
+    assert_eq!(err.kind, ErrorKind::HelpDisplayed);
+    assert_eq!(
+        err.message,
+        EVERYTHING.replace("{{version}}", env!("CARGO_PKG_VERSION"))
+    );
+}
