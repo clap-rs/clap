d12827076f8e11c71695e338282093a8bc0848f9
dylni <46035563+dylni@users.noreply.github.com>
2020-04-22 15:38:14 -0400

    Give `ArgStr` module a better name

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,13 +1,13 @@
+mod argstr;
 mod fnv;
 mod graph;
 mod id;
 mod map;
-mod osstringext;
 mod strext;
 
 pub use self::fnv::Key;
 
-pub(crate) use self::{graph::ChildGraph, id::Id, map::VecMap, osstringext::ArgStr};
+pub(crate) use self::{argstr::ArgStr, graph::ChildGraph, id::Id, map::VecMap};
 
 #[cfg(feature = "color")]
 pub(crate) use termcolor;
