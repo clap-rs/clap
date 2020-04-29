7a7148d08777c93d77636020a2c986f4ae83b1ee
CreepySkeleton <creepy-skeleton@yandex.ru>
2020-04-29 21:34:59 +0300

    Make `struct SubCommand` private

--- src/parse/matches/mod.rs
+++ src/parse/matches/mod.rs
@@ -1,8 +1,9 @@
 mod arg_matches;
 mod matched_arg;
-pub mod subcommand;
 
-pub(crate) use self::matched_arg::{MatchedArg, ValueType};
+pub(crate) use self::{
+    arg_matches::SubCommand,
+    matched_arg::{MatchedArg, ValueType},
+};
 
 pub use self::arg_matches::{ArgMatches, OsValues, Values};
-pub use self::subcommand::SubCommand;
