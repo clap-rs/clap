9477a5ffdb24d45aff36ab587e0691ebbc3fd0a0
Donough Liu <ldm2993593805@163.com>
2020-11-08 01:28:22 +0800

    Correct tests, reveal real problem

--- tests/utils.rs
+++ tests/utils.rs
@@ -47,23 +47,6 @@ pub fn compare_output(l: App, args: &str, right: &str, stderr: bool) -> bool {
     compare(left, right)
 }
 
-pub fn compare_output2(l: App, args: &str, right1: &str, right2: &str, stderr: bool) -> bool {
-    let mut buf = Cursor::new(Vec::with_capacity(50));
-    let res = l.try_get_matches_from(args.split(' ').collect::<Vec<_>>());
-    let err = res.unwrap_err();
-    write!(&mut buf, "{}", err).unwrap();
-    let content = buf.into_inner();
-    let left = String::from_utf8(content).unwrap();
-    assert_eq!(
-        stderr,
-        err.use_stderr(),
-        "Should Use STDERR failed. Should be {} but is {}",
-        stderr,
-        err.use_stderr()
-    );
-    compare(&*left, right1) || compare(&*left, right2)
-}
-
 // Legacy tests from the python script days
 
 pub fn complex_app() -> App<'static> {
