c01df8da409d395af929c337c4d8dd3d5499caf6
Roland Fredenhagen <dev@modprog.de>
2021-09-09 13:04:01 +0200

    doc(generate) fix link in `generate_to`

--- clap_generate/src/lib.rs
+++ clap_generate/src/lib.rs
@@ -42,7 +42,7 @@ pub use shell::Shell;
 /// Generate a completions file for a specified shell at compile-time.
 ///
 /// **NOTE:** to generate the file at compile time you must use a `build.rs` "Build Script" or a
-/// [`cargo-xtask`]](https://github.com/matklad/cargo-xtask)
+/// [`cargo-xtask`](https://github.com/matklad/cargo-xtask)
 ///
 /// # Examples
 ///
