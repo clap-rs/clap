130dcbfdd99e42f6f82d0beede5e4f6f315f6315
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2021-10-26 20:28:35 +0100

    Fixes arg and group with default value having requires

--- src/build/app/mod.rs
+++ src/build/app/mod.rs
@@ -27,7 +27,7 @@ use crate::{
     build::{arg::ArgProvider, Arg, ArgGroup, ArgSettings},
     mkeymap::MKeyMap,
     output::{fmt::Colorizer, Help, HelpWriter, Usage},
-    parse::{ArgMatcher, ArgMatches, Input, Parser},
+    parse::{ArgMatcher, ArgMatches, Input, Parser, ValueType},
     util::{color::ColorChoice, safe_exit, Id, Key, USAGE_CODE},
     Error, ErrorKind, Result as ClapResult, INTERNAL_ERROR_MSG,
 };
@@ -2936,6 +2936,7 @@ impl<'help> App<'help> {
             if let Some(v) = val {
                 if matcher
                     .get(arg)
+                    .filter(|ma| !matches!(ma.ty, ValueType::DefaultValue))
                     .map(|ma| ma.contains_val(v))
                     .unwrap_or(false)
                 {
@@ -2943,8 +2944,10 @@ impl<'help> App<'help> {
                 } else {
                     None
                 }
-            } else {
+            } else if !matcher.is_default_value(arg) {
                 Some(req_arg.clone())
+            } else {
+                None
             }
         };
 
