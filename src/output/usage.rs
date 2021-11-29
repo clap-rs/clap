30f5872a711c8b2126782adf18bfbea86d558851
Ed Page <eopage@gmail.com>
2021-11-29 15:59:56 -0600

    fix: Consistent help between Args and Subcommands
    `App::subcommand_placeholder` encompassed
    - `Arg::value_name`
    - `Arg::help_heading`
    
    Split it and renamed it to make it more consistent.

--- src/output/usage.rs
+++ src/output/usage.rs
@@ -143,7 +143,7 @@ impl<'help, 'app, 'parser> Usage<'help, 'app, 'parser> {
         if self.p.app.has_visible_subcommands() && incl_reqs
             || self.p.is_set(AS::AllowExternalSubcommands)
         {
-            let placeholder = self.p.app.subcommand_placeholder.unwrap_or("SUBCOMMAND");
+            let placeholder = self.p.app.subcommand_value_name.unwrap_or("SUBCOMMAND");
             if self.p.is_set(AS::SubcommandsNegateReqs) || self.p.is_set(AS::ArgsNegateSubcommands)
             {
                 usage.push_str("\n    ");
@@ -194,7 +194,7 @@ impl<'help, 'app, 'parser> Usage<'help, 'app, 'parser> {
         usage.push_str(&*r_string);
         if self.p.is_set(AS::SubcommandRequired) {
             usage.push_str(" <");
-            usage.push_str(self.p.app.subcommand_placeholder.unwrap_or("SUBCOMMAND"));
+            usage.push_str(self.p.app.subcommand_value_name.unwrap_or("SUBCOMMAND"));
             usage.push('>');
         }
         usage.shrink_to_fit();
