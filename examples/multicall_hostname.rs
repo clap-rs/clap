55186f5aa30139ca363b1169749257cad85512bf
Ed Page <eopage@gmail.com>
2021-12-07 15:38:17 -0600

    docs: Highlight subxcommand help heading

--- examples/multicall_hostname.rs
+++ examples/multicall_hostname.rs
@@ -3,6 +3,8 @@ use clap::{App, AppSettings};
 fn main() {
     let app = App::new(env!("CARGO_CRATE_NAME"))
         .setting(AppSettings::ArgRequiredElseHelp)
+        .subcommand_value_name("APPLET")
+        .subcommand_help_heading("APPLETS")
         .subcommand(App::new("hostname").about("show hostname part of FQDN"))
         .subcommand(App::new("dnsdomainname").about("show domain name part of FQDN"));
 
