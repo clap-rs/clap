12c29526faf80164a29c8c1c9f53baf68cc32cc5
Ed Page <eopage@gmail.com>
2022-02-08 15:03:50 -0600

    fix(parser): Give room for more ValueSources

--- src/parse/matches/value_source.rs
+++ src/parse/matches/value_source.rs
@@ -1,5 +1,6 @@
 /// Origin of the argument's value
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+#[non_exhaustive]
 pub enum ValueSource {
     /// Value came [`Arg::default_value`][crate::Arg::default_value]
     DefaultValue,
