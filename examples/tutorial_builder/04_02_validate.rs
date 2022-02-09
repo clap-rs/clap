67192a362d3846889fbb4e2fddb2226ab7f6d4e2
josh rotenberg <joshrotenberg@users.noreply.github.com>
2022-02-08 21:13:15 -0800

    match derive

--- examples/tutorial_builder/04_02_validate.rs
+++ examples/tutorial_builder/04_02_validate.rs
@@ -1,12 +1,24 @@
 use clap::{app_from_crate, arg};
+use std::ops::RangeInclusive;
+use std::str::FromStr;
+
+const PORT_RANGE: RangeInclusive<usize> = 1..=65535;
 
 fn main() {
     let matches = app_from_crate!()
-        .arg(
-            arg!(<PORT>)
-                .help("Network port to use")
-                .validator(|s| s.parse::<usize>()),
-        )
+        .arg(arg!(<PORT>).help("Network port to use").validator(|s| {
+            usize::from_str(s)
+                .map(|port| PORT_RANGE.contains(&port))
+                .map_err(|e| e.to_string())
+                .and_then(|result| match result {
+                    true => Ok(()),
+                    false => Err(format!(
+                        "Port not in range {}-{}",
+                        PORT_RANGE.start(),
+                        PORT_RANGE.end()
+                    )),
+                })
+        }))
         .get_matches();
 
     // Note, it's safe to call unwrap() because the arg is required
