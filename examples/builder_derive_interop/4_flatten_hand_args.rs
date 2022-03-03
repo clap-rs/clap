5d12cbc65a9c69bf7a4b59c3e16f3ee8cd98700a
Matt Fellenz <matt@felle.nz>
2022-03-03 13:10:18 -0800

    Add argument that takes value

--- examples/builder_derive_interop/4_flatten_hand_args.rs
+++ examples/builder_derive_interop/4_flatten_hand_args.rs
@@ -5,6 +5,7 @@ use clap::{Arg, ArgMatches, Args, Command, FromArgMatches, Parser};
 struct CliArgs {
     foo: bool,
     bar: bool,
+    baz: Option<String>,
 }
 
 impl FromArgMatches for CliArgs {
@@ -12,11 +13,15 @@ impl FromArgMatches for CliArgs {
         Ok(Self {
             foo: matches.is_present("foo"),
             bar: matches.is_present("bar"),
+            baz: matches.value_of("baz").map(|baz| baz.to_owned()),
         })
     }
     fn update_from_arg_matches(&mut self, matches: &ArgMatches) -> Result<(), Error> {
         self.foo |= matches.is_present("foo");
         self.bar |= matches.is_present("bar");
+        if let Some(baz) = matches.value_of("baz") {
+            self.baz = Some(baz.to_owned());
+        }
         Ok(())
     }
 }
@@ -25,10 +30,12 @@ impl Args for CliArgs {
     fn augment_args(cmd: Command<'_>) -> Command<'_> {
         cmd.arg(Arg::new("foo").short('f').long("foo"))
             .arg(Arg::new("bar").short('b').long("bar"))
+            .arg(Arg::new("baz").short('B').long("baz").takes_value(true))
     }
     fn augment_args_for_update(cmd: Command<'_>) -> Command<'_> {
         cmd.arg(Arg::new("foo").short('f').long("foo"))
             .arg(Arg::new("bar").short('b').long("bar"))
+            .arg(Arg::new("baz").short('B').long("baz").takes_value(true))
     }
 }
 
