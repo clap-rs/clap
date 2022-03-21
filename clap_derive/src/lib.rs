601a0b426d3d872026682bc58977c00d4cde8044
x0f5c3 <x0f5c3@tutanota.com>
2022-03-21 16:51:51 +0100

    Added proc_macro_attribute for the main function like the one that's used by the Paw crate

--- clap_derive/src/lib.rs
+++ clap_derive/src/lib.rs
@@ -19,7 +19,8 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
-use proc_macro_error::proc_macro_error;
+use proc_macro_error::{abort_call_site, proc_macro_error};
+use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
 
 mod attrs;
@@ -63,4 +64,77 @@ pub fn subcommand(input: TokenStream) -> TokenStream {
 pub fn args(input: TokenStream) -> TokenStream {
     let input: DeriveInput = parse_macro_input!(input);
     derives::derive_args(&input).into()
+}
+
+/// Adds an argument to the main function, the argument type must implement [`clap::Parser`][clap::Parser]
+/// ## Examples
+/// ```rust
+///
+/// #[derive(clap::Parser)]
+/// #[clap(name = "demo")]
+/// struct Context {
+///     /// More verbose output
+///     #[clap(long)]
+///     verbose: bool,
+///     /// An optional name
+///     #[clap(short, long)]
+///     name: Option<String>,
+/// }
+///
+///
+/// #[clap::main]
+/// fn main(args: Context) {
+///     println!("{:?}", args.name);
+/// }
+/// ```
+#[proc_macro_attribute]
+#[proc_macro_error]
+pub fn main(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(item as syn::ItemFn);
+    let ret = &input.sig.output;
+    let name = &input.sig.ident;
+    let body = &input.block;
+    let asyncness = &input.sig.asyncness;
+    let attrs = &input.attrs;
+
+    if name != "main" {
+        abort_call_site!("only fn main can be tagged with #[clap::main]");
+    }
+    let end = match ret {
+        syn::ReturnType::Default => quote! {.unwrap()},
+        _ => quote! {?},
+    };
+    let inputs = &input.sig.inputs;
+    let result = match inputs.len() {
+        0 => {
+            quote! {
+                #(#attrs)*
+                #asyncness fn main() #ret {
+                    #body
+                }
+            }
+        }
+        1 => {
+            let arg = match inputs.first().unwrap() {
+                syn::FnArg::Typed(arg) => arg,
+                _ => {
+                    abort_call_site!("fn main should take a fully formed argument");
+                }
+            };
+            let arg_name = &arg.pat;
+            let arg_type = &arg.ty;
+            quote! {
+                #(#attrs)*
+                #asyncness fn main() #ret {
+                    let #arg_name = <#arg_type as clap::Parser>::try_parse()#end;
+                    #body
+                }
+
+            }
+        }
+        _ => {
+            abort_call_site!("fn main can take 0 or 1 arguments");
+        }
+    };
+    result.into()
 }
