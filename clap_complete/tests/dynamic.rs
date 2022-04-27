4b9dd3fbbb85424826f402eb4ee8cc6c78a96718
Ed Page <eopage@gmail.com>
2022-04-27 15:40:49 -0500

    test(complete): Cover long flags

--- clap_complete/tests/dynamic.rs
+++ clap_complete/tests/dynamic.rs
@@ -35,6 +35,52 @@ fn suggest_subcommand_subset() {
 }
 
 #[test]
+fn suggest_long_flag_subset() {
+    let name = "test";
+    let mut cmd = clap::Command::new(name)
+        .arg(
+            clap::Arg::new("hello-world")
+                .long("--hello-world")
+                .multiple_occurrences(true),
+        )
+        .arg(
+            clap::Arg::new("hello-moon")
+                .long("--hello-moon")
+                .multiple_occurrences(true),
+        )
+        .arg(
+            clap::Arg::new("goodbye-world")
+                .long("--goodbye-world")
+                .multiple_occurrences(true),
+        );
+
+    let args = [name, "--he"];
+    let arg_index = 1;
+    let args = IntoIterator::into_iter(args)
+        .map(|s| std::ffi::OsString::from(s))
+        .collect::<Vec<_>>();
+    let comp_type = clap_complete::dynamic::bash::CompType::default();
+    let trailing_space = true;
+    let current_dir = None;
+
+    let completions = clap_complete::dynamic::bash::complete(
+        &mut cmd,
+        args,
+        arg_index,
+        comp_type,
+        trailing_space,
+        current_dir,
+    )
+    .unwrap();
+    let completions = completions
+        .into_iter()
+        .map(|s| s.to_string_lossy().into_owned())
+        .collect::<Vec<_>>();
+
+    assert_eq!(completions, ["--help", "--hello-world", "--hello-moon"]);
+}
+
+#[test]
 fn suggest_additional_short_flags() {
     let name = "test";
     let mut cmd = clap::Command::new(name)
