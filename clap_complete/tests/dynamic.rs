c9b6d62ba76bb0c19875e902a2942d245afee145
Ed Page <eopage@gmail.com>
2022-04-27 15:40:24 -0500

    test(complete): Cover subcommands

--- clap_complete/tests/dynamic.rs
+++ clap_complete/tests/dynamic.rs
@@ -1,6 +1,40 @@
 #![cfg(feature = "unstable-dynamic")]
 
 #[test]
+fn suggest_subcommand_subset() {
+    let name = "test";
+    let mut cmd = clap::Command::new(name)
+        .subcommand(clap::Command::new("hello-world"))
+        .subcommand(clap::Command::new("hello-moon"))
+        .subcommand(clap::Command::new("goodbye-world"));
+
+    let args = [name, "he"];
+    let arg_index = 1;
+    let args = IntoIterator::into_iter(args)
+        .map(|s| std::ffi::OsString::from(s))
+        .collect::<Vec<_>>();
+    let comp_type = clap_complete::dynamic::bash::CompType::default();
+    let trailing_space = true;
+    let current_dir = None;
+
+    let completions = clap_complete::dynamic::bash::complete(
+        &mut cmd,
+        args,
+        arg_index,
+        comp_type,
+        trailing_space,
+        current_dir,
+    )
+    .unwrap();
+    let completions = completions
+        .into_iter()
+        .map(|s| s.to_string_lossy().into_owned())
+        .collect::<Vec<_>>();
+
+    assert_eq!(completions, ["hello-moon", "hello-world", "help"]);
+}
+
+#[test]
 fn suggest_additional_short_flags() {
     let name = "test";
     let mut cmd = clap::Command::new(name)
