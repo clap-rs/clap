3d64ebacac70245a40ee8df6b0f6b198f1cc5561
Ed Page <eopage@gmail.com>
2022-04-27 08:54:39 -0500

    test(complete): Option to verify examples

---
+++ clap_complete/tests/examples.rs
@@ -0,0 +1,13 @@
+#![cfg(not(tarpaulin))]
+
+#[test]
+fn example_tests() {
+    let t = trycmd::TestCases::new();
+    let features: &[&str] = &[
+        #[cfg(feature = "unstable-dynamic")]
+        "unstable-dynamic",
+    ];
+    let features = features.join(" ");
+    t.register_bins(trycmd::cargo::compile_examples(["--features", &features]).unwrap());
+    t.case("examples/**/*.md");
+}
