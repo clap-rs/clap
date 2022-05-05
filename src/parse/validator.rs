f7c2deaa476e74f801bb080fc9de018cec8f43a2
Ed Page <eopage@gmail.com>
2022-05-05 12:14:46 -0500

    fix(help): Don't show hidden arguments for conflicts
    This makes it consistent with other errors where we show arguments to
    the user.

--- src/parse/validator.rs
+++ src/parse/validator.rs
@@ -290,6 +290,10 @@ impl<'help, 'cmd> Validator<'help, 'cmd> {
         let used_filtered: Vec<Id> = matcher
             .arg_names()
             .filter(|arg_id| matcher.check_explicit(arg_id, ArgPredicate::IsPresent))
+            .filter(|n| {
+                // Filter out the args we don't want to specify.
+                self.cmd.find(n).map_or(true, |a| !a.is_hide_set())
+            })
             .filter(|key| !conflicting_keys.contains(key))
             .cloned()
             .collect();
