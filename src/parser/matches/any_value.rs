f16a6fb5a6b7dd1f3e9b6ce6d9bff35204267bd1
Ed Page <eopage@gmail.com>
2022-05-13 16:09:27 -0500

    refactor(parser): Move AnyValue definition closer to use

---
+++ src/parser/matches/any_value.rs
@@ -0,0 +1,2 @@
+pub(crate) type AnyValue = std::sync::Arc<dyn std::any::Any + Send + Sync + 'static>;
+
--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -9,10 +9,9 @@ mod validator;
 pub(crate) mod features;
 
 pub(crate) use self::arg_matcher::ArgMatcher;
+pub(crate) use self::matches::AnyValue;
 pub(crate) use self::matches::{MatchedArg, SubCommand};
 pub(crate) use self::parser::{ParseState, Parser};
 pub(crate) use self::validator::Validator;
 
 pub use self::matches::{ArgMatches, Indices, OsValues, ValueSource, Values};
-
-pub(crate) type AnyValue = std::sync::Arc<dyn std::any::Any + Send + Sync + 'static>;
