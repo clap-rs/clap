a2871b0858345c672bd74360dcbe4a6a5ca7e715
邹伟 <835683985@qq.com>
2022-06-11 09:18:32 +0800

    fix:default_value_ifs_os modified arg name, it replaces arg with arg.key(), but it call self.default_value_if_os. so please delete `.key()`.

--- tests/builder/arg_settings.rs
+++ tests/builder/arg_settings.rs
@@ -1,5 +1,7 @@
 #![allow(deprecated)]
-use clap::{Arg, ArgSettings};
+
+use std::ffi::OsStr;
+use clap::{Arg, ArgSettings, Command};
 
 #[test]
 fn setting() {
@@ -41,4 +43,38 @@ fn unset_setting_bitor() {
     assert!(!m.is_required_set(), "{:#?}", m);
     assert!(!m.is_hide_set(), "{:#?}", m);
     assert!(!m.is_last_set(), "{:#?}", m);
+}
+
+#[test]
+fn default_value_ifs_os() {
+    let cmd = Command::new("my_cargo")
+        .arg(
+            Arg::new("flag")
+                .long("flag")
+                .allow_invalid_utf8(true)
+                .takes_value(true)
+        )
+        .arg(
+            arg!(option: -o --option [option] "option arg")
+                .allow_invalid_utf8(true)
+        )
+        .arg(
+            Arg::new("other")
+                .long("other")
+                .allow_invalid_utf8(true)
+                .default_value_ifs_os(&[
+                    ("flag", Some("标记2").map(OsStr::new), Some("flag=标记2").map(OsStr::new))
+                ])
+        );
+    let result = cmd.try_get_matches_from([OsStr::new("my_cargo"), OsStr::new("--flag"), OsStr::new("标记2")]);
+    assert!(result.is_ok());
+    match result {
+        Ok(arg_matches) => {
+            assert_eq!(arg_matches.value_of_os("flag"), Some(OsStr::new("标记2")));
+            assert_eq!(arg_matches.value_of_os("other"), Some(OsStr::new("flag=标记2")));
+        }
+        Err(e) => {
+            println!("{}", e.to_string());
+        }
+    }
 }
