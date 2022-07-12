9b6321a1f0128ea09f59f1abc169c79b97597dff
Ed Page <eopage@gmail.com>
2022-07-11 21:48:07 -0500

    chore: Release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -30,6 +30,8 @@ _gated behind `unstable-v4`_
 <!-- next-header -->
 ## [Unreleased] - ReleaseDate
 
+## [3.2.10] - 2022-07-12
+
 ### Fixes
 
 - Loosen lifetime on `Command::mut_subcommand`
@@ -3505,7 +3507,8 @@ Minimum version of Rust is now v1.13.0 (Stable)
 * **arg**  allow lifetimes other than 'static in arguments ([9e8c1fb9](https://github.com/clap-rs/clap/commit/9e8c1fb9406f8448873ca58bab07fe905f1551e5))
 
 <!-- next-url -->
-[Unreleased]: https://github.com/clap-rs/clap/compare/v3.2.8...HEAD
+[Unreleased]: https://github.com/clap-rs/clap/compare/v3.2.10...HEAD
+[3.2.10]: https://github.com/clap-rs/clap/compare/v3.2.8...v3.2.10
 [3.2.8]: https://github.com/clap-rs/clap/compare/v3.2.7...v3.2.8
 [3.2.7]: https://github.com/clap-rs/clap/compare/v3.2.6...v3.2.7
 [3.2.6]: https://github.com/clap-rs/clap/compare/v3.2.5...v3.2.6
--- src/derive.rs
+++ src/derive.rs
@@ -20,7 +20,7 @@ use std::ffi::OsString;
 /// See also [`Subcommand`] and [`Args`].
 ///
 /// See the
-/// [derive reference](https://github.com/clap-rs/clap/blob/v3.2.8/examples/derive_ref/README.md)
+/// [derive reference](https://github.com/clap-rs/clap/blob/v3.2.10/examples/derive_ref/README.md)
 /// for attributes and best practices.
 ///
 /// **NOTE:** Deriving requires the `derive` feature flag
@@ -373,7 +373,7 @@ pub trait FromArgMatches: Sized {
 /// - `Variant(ChildArgs)`: No attribute is used with enum variants that impl `Args`.
 ///
 /// See the
-/// [derive reference](https://github.com/clap-rs/clap/blob/v3.2.8/examples/derive_ref/README.md)
+/// [derive reference](https://github.com/clap-rs/clap/blob/v3.2.10/examples/derive_ref/README.md)
 /// for attributes and best practices.
 ///
 /// **NOTE:** Deriving requires the `derive` feature flag
@@ -417,7 +417,7 @@ pub trait Args: FromArgMatches + Sized {
 ///   `Subcommand`.
 ///
 /// See the
-/// [derive reference](https://github.com/clap-rs/clap/blob/v3.2.8/examples/derive_ref/README.md)
+/// [derive reference](https://github.com/clap-rs/clap/blob/v3.2.10/examples/derive_ref/README.md)
 /// for attributes and best practices.
 ///
 /// **NOTE:** Deriving requires the `derive` feature flag
@@ -461,7 +461,7 @@ pub trait Subcommand: FromArgMatches + Sized {
 /// - Allowing using the `#[clap(default_value_t)]` attribute without implementing `Display`.
 ///
 /// See the
-/// [derive reference](https://github.com/clap-rs/clap/blob/v3.2.8/examples/derive_ref/README.md)
+/// [derive reference](https://github.com/clap-rs/clap/blob/v3.2.10/examples/derive_ref/README.md)
 /// for attributes and best practices.
 ///
 /// **NOTE:** Deriving requires the `derive` feature flag
