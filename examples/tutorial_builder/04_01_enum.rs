36777e7b6ca0e8224e2f9a1c87d17ef8e9663d10
Ed Page <eopage@gmail.com>
2022-08-08 14:12:44 -0500

    docs(tutorial): Switch to hand-implemented ValueEnum

--- examples/tutorial_builder/04_01_enum.rs
+++ examples/tutorial_builder/04_01_enum.rs
@@ -1,9 +1,45 @@
-use clap::{arg, command, value_parser, ValueEnum};
+use clap::{arg, builder::PossibleValue, command, value_parser, ValueEnum};
 
-#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)] // requires `derive` feature
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
 enum Mode {
     Fast,
     Slow,
+}
+
+// Can also be derived] with feature flag `derive`
+impl ValueEnum for Mode {
+    fn value_variants<'a>() -> &'a [Self] {
+        &[Mode::Fast, Mode::Slow]
+    }
+
+    fn to_possible_value<'a>(&self) -> Option<PossibleValue<'a>> {
+        Some(match self {
+            Mode::Fast => PossibleValue::new("fast"),
+            Mode::Slow => PossibleValue::new("slow"),
+        })
+    }
+}
+
+impl std::fmt::Display for Mode {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.to_possible_value()
+            .expect("no values are skipped")
+            .get_name()
+            .fmt(f)
+    }
+}
+
+impl std::str::FromStr for Mode {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        for variant in Self::value_variants() {
+            if variant.to_possible_value().unwrap().matches(s, false) {
+                return Ok(*variant);
+            }
+        }
+        Err(format!("Invalid variant: {}", s))
+    }
 }
 
 fn main() {
