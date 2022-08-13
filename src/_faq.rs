ec55eb6a20cef0633de7a7eebec4ce7167a77e0d
Chris Pryer <cnpryer@gmail.com>
2022-08-13 19:28:16 -0400

    docs: Quality improvements to FAQ docs

--- src/_faq.rs
+++ src/_faq.rs
@@ -15,7 +15,7 @@
 //! in a critical or harsh manner. All the argument parsing libraries out there (to
 //! include `clap`) have their own strengths and weaknesses. Sometimes it just
 //! comes down to personal taste when all other factors are equal. When in doubt,
-//! try them all and pick one that you enjoy :) There's plenty of room in the Rust
+//! try them all and pick one that you enjoy :). There's plenty of room in the Rust
 //! community for multiple implementations!
 //!
 //! For less detailed but more broad comparisons, see
@@ -42,19 +42,19 @@
 //!
 //! #### What are some reasons to use `clap`? (The Pitch)
 //!
-//! `clap` is as fast, and as lightweight as possible while still giving all the features you'd expect from a modern argument parser. In fact, for the amount and type of features `clap` offers it remains about as fast as `getopts`. If you use `clap` when just need some simple arguments parsed, you'll find it's a walk in the park. `clap` also makes it possible to represent extremely complex, and advanced requirements, without too much thought. `clap` aims to be intuitive, easy to use, and fully capable for wide variety use cases and needs.
+//! `clap` is as fast, and as lightweight as possible while still giving all the features you'd expect from a modern argument parser. In fact, for the amount and type of features `clap` offers it remains about as fast as `getopts`. If you use `clap`, when you just need some simple arguments parsed, you'll find it's a walk in the park. `clap` also makes it possible to represent extremely complex and advanced requirements without too much thought. `clap` aims to be intuitive, easy to use, and fully capable for wide variety use cases and needs.
 //!
 //! #### What are some reasons *not* to use `clap`? (The Anti Pitch)
 //!
-//! Depending on the style in which you choose to define the valid arguments, `clap` can be very verbose. `clap` also offers so many finetuning knobs and dials, that learning everything can seem overwhelming. I strive to keep the simple cases simple, but when turning all those custom dials it can get complex. `clap` is also opinionated about parsing. Even though so much can be tweaked and tuned with `clap` (and I'm adding more all the time), there are still certain features which `clap` implements in specific ways which may be contrary to some users use-cases.
+//! Depending on the style in which you choose to define the valid arguments, `clap` can be very verbose. `clap` also offers so many finetuning knobs and dials, that learning everything can seem overwhelming. I strive to keep the simple cases simple, but when turning all those custom dials it can get complex. `clap` is also opinionated about parsing. Even though so much can be tweaked and tuned with `clap` (and I'm adding more all the time), there are still certain features which `clap` implements in specific ways that may be contrary to some users' use-cases.
 //!
 //! #### Reasons to use `clap`
 //!
-//!  * You want all the nice CLI features your users may expect, yet you don't want to implement them all yourself. You'd like to focus your application, not argument parsing.
-//!  * In addition to the point above; you don't want to sacrifice performance to get all those nice features
+//!  * You want all the nice CLI features your users may expect, yet you don't want to implement them all yourself. You'd like to focus on your application, not argument parsing.
+//!  * In addition to the point above, you don't want to sacrifice performance to get all those nice features.
 //!  * You have complex requirements/conflicts between your various valid args.
-//!  * You want to use subcommands (although other libraries also support subcommands, they are not nearly as feature rich as those provided by `clap`)
-//!  * You want some sort of custom validation built into the argument parsing process, instead of as part of your application (which allows for earlier failures, better error messages, more cohesive experience, etc.)
+//!  * You want to use subcommands (although other libraries also support subcommands, they are not nearly as feature rich as those provided by `clap`).
+//!  * You want some sort of custom validation built into the argument parsing process, instead of as part of your application (which allows for earlier failures, better error messages, more cohesive experience, etc.).
 //!
 //! ### How many approaches are there to create a parser?
 //!
