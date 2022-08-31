803d87b1938f616657a0543eaf934ca3603b6b3c
Ed Page <eopage@gmail.com>
2022-08-31 15:39:15 -0500

    refactor(help): Reduce magic numbers

--- src/output/mod.rs
+++ src/output/mod.rs
@@ -8,3 +8,6 @@ pub(crate) use self::help::Help;
 pub(crate) use self::textwrap::core::display_width;
 pub(crate) use self::textwrap::wrap;
 pub(crate) use self::usage::Usage;
+
+pub(crate) const TAB: &str = "    ";
+pub(crate) const TAB_WIDTH: usize = TAB.len();
