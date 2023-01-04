ccce3c3db5ce9f59d52012c0a0e22988de2f9b9a
Thayne McCombs <astrothayne@gmail.com>
2023-01-04 00:51:51 -0700

    test: Add test for opting out of Vec<Vec<T>> derive

--- tests/derive/occurrences.rs
+++ tests/derive/occurrences.rs
@@ -18,6 +18,28 @@ fn test_vec_of_vec() {
 }
 
 #[test]
+fn test_vec_of_vec_opt_out() {
+    fn parser(s: &str) -> Result<Vec<Vec<String>>, std::convert::Infallible> {
+        Ok(s.split(':')
+            .map(|v| v.split(',').map(str::to_owned).collect())
+            .collect())
+    }
+
+    #[derive(Parser, PartialEq, Debug)]
+    struct Opt {
+        #[arg(value_parser = parser, short = 'p')]
+        arg: ::std::vec::Vec<Vec<String>>,
+    }
+
+    assert_eq!(
+        Opt {
+            arg: vec![vec!["1".into(), "2".into()], vec!["a".into(), "b".into()]],
+        },
+        Opt::try_parse_from(["test", "-p", "1,2:a,b"]).unwrap(),
+    );
+}
+
+#[test]
 fn test_vec_vec_empty() {
     #[derive(Parser, Debug, PartialEq)]
     struct Opt {
