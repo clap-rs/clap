f25fe5b3e014fcfa58b965ccfb5b607a33c3402b
Ed Page <eopage@gmail.com>
2023-01-09 12:29:41 -0600

    chore: Bump MSRV to 1.64.0

--- tests/derive_ui.rs
+++ tests/derive_ui.rs
@@ -6,7 +6,7 @@
 // option. This file may not be copied, modified, or distributed
 
 #[cfg(feature = "derive")]
-#[rustversion::attr(any(not(stable), before(1.60), since(1.61)), ignore)] // MSRV
+#[rustversion::attr(any(not(stable), before(1.64), since(1.65)), ignore)] // MSRV
 #[test]
 fn ui() {
     let t = trybuild::TestCases::new();
