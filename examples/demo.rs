f190238795e29c53caec158abf5de877969c3be0
Jason Ivey <jason.ivey.not.micheal@gmail.com>
2023-07-20 17:19:23 -0700

    Update demo.rs
    Added comment mentioning derive requirement. 
    
    Many bad programmers (like me) copy the example on doc.rs and don't read the text surrounding the code, this gives them (us) a second chance.

--- examples/demo.rs
+++ examples/demo.rs
@@ -1,6 +1,7 @@
 use clap::Parser;
 
 /// Simple program to greet a person
+/// Requires feature "derive" 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
