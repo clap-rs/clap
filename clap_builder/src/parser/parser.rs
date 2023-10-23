9a9aabc178b3d28d534b867e171bd0ef6ab0f148
Alex Crichton <alex@alexcrichton.com>
2023-10-23 12:29:17 -0700

    refactor: Reduce code size of testing tokens if they're a number
    This commit is a tiny win in compiled code size of a final binary
    including `clap` which shaves off 19k of compiled code locally.
    Previously tokens were checked if they were a number by using
    `.parse::<f64>().is_ok()`, but parsing floats is relatively heavyweight
    in terms of code size. This replaces the check with a more naive "does
    this string have lots of ascii digits" check where the compiled size of
    this check should be much smaller.

--- clap_builder/src/parser/parser.rs
+++ clap_builder/src/parser/parser.rs
@@ -643,7 +643,7 @@ impl<'cmd> Parser<'cmd> {
 
         if self.cmd[current_positional.get_id()].is_allow_hyphen_values_set()
             || (self.cmd[current_positional.get_id()].is_allow_negative_numbers_set()
-                && next.is_number())
+                && next.is_negative_number())
         {
             // If allow hyphen, this isn't a new arg.
             debug!("Parser::is_new_arg: Allow hyphen");
@@ -841,7 +841,7 @@ impl<'cmd> Parser<'cmd> {
 
         #[allow(clippy::blocks_in_if_conditions)]
         if matches!(parse_state, ParseState::Opt(opt) | ParseState::Pos(opt)
-                if self.cmd[opt].is_allow_hyphen_values_set() || (self.cmd[opt].is_allow_negative_numbers_set() && short_arg.is_number()))
+                if self.cmd[opt].is_allow_hyphen_values_set() || (self.cmd[opt].is_allow_negative_numbers_set() && short_arg.is_negative_number()))
         {
             debug!("Parser::parse_short_args: prior arg accepts hyphenated values",);
             return Ok(ParseResult::MaybeHyphenValue);
@@ -851,7 +851,7 @@ impl<'cmd> Parser<'cmd> {
             .get(&pos_counter)
             .map(|arg| arg.is_allow_negative_numbers_set())
             .unwrap_or_default()
-            && short_arg.is_number()
+            && short_arg.is_negative_number()
         {
             debug!("Parser::parse_short_arg: negative number");
             return Ok(ParseResult::MaybeHyphenValue);
