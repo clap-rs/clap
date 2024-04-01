ebc70d00f9259146592b7987bfcb8a0cb6c16661
Ed Page <eopage@gmail.com>
2024-04-01 09:11:24 -0500

    chore: Only check missing_docs in lib
    This also fires in examples and other places.
    
    While docs in examples would be nice,
    it isn't universally applicable and `allow`s would undermine the
    examples.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 //! > DESCRIPTION
 
 #![cfg_attr(docsrs, feature(doc_auto_cfg))]
+#![warn(missing_docs)]
 #![warn(clippy::print_stderr)]
 #![warn(clippy::print_stdout)]
 #![allow(non_snake_case)] // TODO: Delete me
