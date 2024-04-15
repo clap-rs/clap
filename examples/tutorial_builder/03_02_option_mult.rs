d9081f3dcead46c092ff93e0a630a763852cfcde
zippy-dice <zippy_dice@proton.me>
2024-04-16 02:11:09 +0900

    docs(exmaples): Fix example to better align with intended use case

--- examples/tutorial_builder/03_02_option_mult.rs
+++ examples/tutorial_builder/03_02_option_mult.rs
@@ -10,5 +10,11 @@ fn main() {
         )
         .get_matches();
 
-    println!("name: {:?}", matches.get_one::<String>("name"));
+    let args = matches
+        .get_many::<String>("name")
+        .unwrap_or_default()
+        .map(|v| v.as_str())
+        .collect::<Vec<_>>();
+
+    println!("names: {:?}", &args);
 }
