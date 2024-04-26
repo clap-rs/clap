a01f25da96e8bd3e216fbc19ac9883ab3bf969ce
Ed Page <eopage@gmail.com>
2024-04-26 09:23:28 -0500

    chore(ci): Reduce noisy lints
    Want to add this back in later but this is slowing down migration of my
    repos.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 //! > DESCRIPTION
 
 #![cfg_attr(docsrs, feature(doc_auto_cfg))]
-#![warn(missing_docs)]
 #![warn(clippy::print_stderr)]
 #![warn(clippy::print_stdout)]
 #![allow(non_snake_case)] // TODO: Delete me
