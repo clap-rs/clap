c46facdcd11feba6dad28859b2797fb718506940
initializedd <119122691+initializedd@users.noreply.github.com>
2024-12-21 01:47:26 +0000

    feat: Integrate initial OSS-Fuzz support (#5850)

---
+++ fuzz/fuzz_targets/parse_fuzzer.rs
@@ -0,0 +1,27 @@
+#![no_main]
+
+use libfuzzer_sys::fuzz_target;
+use fuzzed_data_provider_rs::FuzzedDataProvider;
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+    #[arg(short, long)]
+    test: String,
+
+    #[arg(short, long)]
+    param: String,
+
+    #[arg(short, long)]
+    name: String,
+}
+
+fuzz_target!(|data: &[u8]| {
+    let mut fdp = FuzzedDataProvider::new(data);
+    let _ = Args::try_parse_from(vec![
+        fdp.consume_random_length_string(fdp.remaining_bytes()),
+        fdp.consume_random_length_string(fdp.remaining_bytes()),
+        fdp.consume_random_length_string(fdp.remaining_bytes()),
+    ]);
+});
+
