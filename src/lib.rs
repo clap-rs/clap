d159ec5290740054b9b344f995d453d37307e7ff
Ed Page <eopage@gmail.com>
2025-01-24 11:54:12 -0600

    test: Verify readme

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,7 @@
 #![warn(clippy::print_stderr)]
 #![warn(clippy::print_stdout)]
 #![allow(non_snake_case)] // TODO: Delete me
+
+#[doc = include_str!("../README.md")]
+#[cfg(doctest)]
+pub struct ReadmeDoctests;
