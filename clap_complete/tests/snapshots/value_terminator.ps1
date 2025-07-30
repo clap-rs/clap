7b93a9a45743a677eeacfa5cb7672dffed230d26
Matej Kafka <git@a-n.cc>
2025-07-30 17:03:48 +0200

    feat(complete): Only complete flags in PowerShell when word starts with -

--- clap_complete/tests/snapshots/value_terminator.ps1
+++ clap_complete/tests/snapshots/value_terminator.ps1
@@ -27,6 +27,10 @@ Register-ArgumentCompleter -Native -CommandName 'my-app' -ScriptBlock {
         }
     })
 
+    if ($wordToComplete -notlike "-*") {
+        $completions = $completions.Where{ $_.CompletionText -notlike "-*" }
+    }
+
     $completions.Where{ $_.CompletionText -like "$wordToComplete*" } |
         Sort-Object -Property ListItemText
 }
