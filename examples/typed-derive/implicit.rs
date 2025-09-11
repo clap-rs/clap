8f8e8613459e3ccdd25051c97f011cd8d5e49ed9
Ed Page <eopage@gmail.com>
2025-09-11 11:54:37 -0500

    docs(cookbook): Add local enum to typed-derive

--- examples/typed-derive/implicit.rs
+++ examples/typed-derive/implicit.rs
@@ -1,4 +1,5 @@
 use clap::Args;
+use clap::ValueEnum;
 
 #[derive(Args, Debug)]
 pub(crate) struct ImplicitParsers {
@@ -17,4 +18,45 @@ pub(crate) struct ImplicitParsers {
     /// Allow human-readable durations
     #[arg(long)]
     sleep: Option<jiff::SignedDuration>,
+
+    /// Custom enums
+    #[arg(long)]
+    bump_level: Option<BumpLevel>,
+}
+
+#[derive(Debug, Clone, Copy, ValueEnum)]
+#[value(rename_all = "kebab-case")]
+pub(crate) enum BumpLevel {
+    /// Increase the major version (x.0.0)
+    Major,
+    /// Increase the minor version (x.y.0)
+    Minor,
+    /// Increase the patch version (x.y.z)
+    Patch,
+}
+
+impl std::fmt::Display for BumpLevel {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        use clap::ValueEnum;
+
+        self.to_possible_value()
+            .expect("no values are skipped")
+            .get_name()
+            .fmt(f)
+    }
+}
+
+impl std::str::FromStr for BumpLevel {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        use clap::ValueEnum;
+
+        for variant in Self::value_variants() {
+            if variant.to_possible_value().unwrap().matches(s, false) {
+                return Ok(*variant);
+            }
+        }
+        Err(format!("Invalid variant: {s}"))
+    }
 }
