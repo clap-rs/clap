f218f4ae7ae9d079e52014b7f4a242cff9da89d4
Ed Page <eopage@gmail.com>
2025-09-29 11:37:57 -0500

    docs(docsrs): Update to new doc_cfg feature name

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 //! > DESCRIPTION
 
-#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+#![cfg_attr(docsrs, feature(doc_cfg))]
 #![warn(clippy::print_stderr)]
 #![warn(clippy::print_stdout)]
 #![allow(non_snake_case)] // TODO: Delete me
