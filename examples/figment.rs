7bed31a56eeaa43dac14f58d6b1b61416f100d17
grbd <hecatrons.workshop@gmail.com>
2025-10-25 19:40:16 +0100

    docs: Added more fixes for the rustfmt

--- examples/figment.rs
+++ examples/figment.rs
@@ -1,14 +1,16 @@
-use std::path::PathBuf;
-use clap::{CommandFactory, Parser, error::ErrorKind};
-use figment::{Figment, providers::{Serialized, Toml, Format}};
-use serde::{Serialize, Deserialize};
+use clap::{error::ErrorKind, CommandFactory, Parser};
+use figment::{
+    providers::{Format, Serialized, Toml},
+    Figment,
+};
+use serde::{Deserialize, Serialize};
 use serde_default_utils::*;
+use std::path::PathBuf;
 
 /// A demo showing the use of figment with clap
 #[serde_inline_default]
 #[derive(Parser, Debug, Serialize, Deserialize)]
 pub struct AppConfig {
-
     /// Name of the person to greet
     #[arg(short, long)]
     #[serde(skip_serializing_if = "::std::option::Option::is_none")]
@@ -22,21 +24,21 @@ pub struct AppConfig {
 }
 
 fn main() {
-    let cfgpath = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("examples").join("figment_config.toml");
+    let cfgpath = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+        .join("examples")
+        .join("figment_config.toml");
 
     let config: AppConfig = Figment::new()
         .merge(Toml::file(cfgpath))
         .merge(Serialized::defaults(AppConfig::parse()))
-        .extract().unwrap();
+        .extract()
+        .unwrap();
 
     // Custom Validation
     if config.name.is_none() {
         let mut cmd = AppConfig::command();
-        cmd.error(
-            ErrorKind::MissingRequiredArgument,
-            "name option not found",
-        )
-        .exit();
+        cmd.error(ErrorKind::MissingRequiredArgument, "name option not found")
+            .exit();
     }
 
     println!("Name: {}", config.name.unwrap());
